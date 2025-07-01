854889769ae6740e9106b33cd6f5caff306d5505
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2025-06-30 17:18:50 -0700

    update form to include message and raw timestamp

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -3,32 +3,49 @@ import React, { useState } from "react";
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const timestamp = new Date().toISOString();
     const res = await fetch("/api/data", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email }),
+      body: JSON.stringify({ name, email, message, timestamp }),
     });
     const result = await res.json();
     console.log(result);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <form className="ticket-form" onSubmit={handleSubmit}>
+      <h2>Create Ticket</h2>
+      <div className="form-field">
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-field">
+        <input
+          type="message"
+          rows="4"
+          placeholder="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
       <button type="submit">Send</button>
     </form>
   );
--- src/Pages/IssuesPage.jsx
+++ src/Pages/IssuesPage.jsx
@@ -1,10 +1,13 @@
+import ContactForm from "../Components/ContactForm/ContactForm";
 import Issues from "../Components/Issues/Issues";
 import Nav from "../Components/Nav/Nav";
 function IssuesPage() {
   return (
     <>
       <Nav />
+
       <Issues />
+      <ContactForm />
     </>
   );
 }
