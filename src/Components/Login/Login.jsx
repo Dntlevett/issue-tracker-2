a7d41ae8789dc29f7b759e137d07b051404006d7
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2025-12-24 18:49:36 -0800

    create login jsx page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,3 +1,46 @@
-import { usestate } from "react";
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import "./login.scss";
 
-n;
+export default function Login() {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+
+    if (data.token) {
+      login(data.token);
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+  return (
+    <div className="login-container">
+      <form className="login-box" onSubmit={handleSubmit}>
+        <h2>Sign in</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
